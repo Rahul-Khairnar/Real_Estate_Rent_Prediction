f656da80bea7b5a98de353d4486e059252be6f76
Rahul-Khairnar <rahulkhairnar10e@gmail.com>
2020-07-23 11:09:28 +0530

    Adding updated server codes for up and running server

--- server/Server.py
+++ server/Server.py
@@ -9,16 +9,30 @@ import util
 
 app = Flask(__name__)
 ## DEFINING THE HTTP END POINT. THE BELOW IS THE CODE FOR ROUTINE
-@app.route('/hello')
-## IMPORTING THE JSON FILES WITH ALL THE LOCATION
+
+@app.route('/get_location_names', methods=['GET'])
 def get_location_names():
     response = jsonify({
-        'location':util.get_location_names()
-        })
-    response.headers.add('Access-Control-Allow-Origin','*')
-    return response
-    return 'Hi!'
+        'locations': util.get_location_names()
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
 
+    return response
+
+@app.route('/predict_home_rent',methods=['GET','POST'])
+def predict_home_rent():
+    area = float(request.form['area'])
+    locality = request.form['locality']
+    bathroom_num = int(request.form['bathroom_num'])
+    bedroom_num = int(request.form['bedroom_num'])
+    
+    response = jsonify({
+        'estimated_price':util.get_estimated_rent(locality,area,bathroom_num,bedroom_num)})
+   
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+    
 if __name__ == '__main__':
     print("Starting the FLask Server for Property Rent Production....")
+    util.load_saved_artifacts()
     app.run()
--- server/util.py
+++ server/util.py
@@ -12,16 +12,16 @@ __locations = None
 __data_columns = None
 __model = None
 
-def get_estimated_rent(location,area,bathrooms,bedrooms):
+def get_estimated_rent(locality,area,bathroom_num,bedroom_num):
     try:
-        loc_index = __data_columns.index(location.lower())
+        loc_index = __data_columns.index(locality.lower())
     except:
         loc_index = -1
     
     x = np.zeros(len(__data_columns))
     x[0] = area
-    x[1] = bathrooms
-    x[2] = bedrooms
+    x[1] = bathroom_num
+    x[2] = bedroom_num
     if loc_index >= 0:
         x[loc_index] = 1
     return round(__model.predict([x])[0],2)
@@ -31,9 +31,10 @@ def load_saved_artifacts():
     global __data_columns
     global __locations
     
-    with open("E:\PROJECTS\REAL_ESTATE_RENT_PREDICTION\server\Artifacts\columns.json","rb") as f:
+    with open("E:\PROJECTS\REAL_ESTATE_RENT_PREDICTION\server\Artifacts\columns.json","r") as f:
         __data_columns = json.load(f)['data_columns']
         __locations = __data_columns[3:]
+        
     global __model
     if __model is None:    
         with open("E:\PROJECTS\REAL_ESTATE_RENT_PREDICTION\server\Artifacts\Mumbai_house_rent_prediction_model.pickle","rb") as f:
